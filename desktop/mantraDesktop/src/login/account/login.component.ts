25a7e439c704643ac945bd190c5c036423a6bf08
Lukáš Obertáš <lukas.obertas@student.tuke.sk>
2020-11-22 21:02:17 +0100

    fix nepotrebnych veci

--- desktop/mantraDesktop/src/login/account/login.component.ts
+++ desktop/mantraDesktop/src/login/account/login.component.ts
@@ -30,31 +30,4 @@ export class LoginComponent implements OnInit {
     // get return url from route parameters or default to '/'
     this.returnUrl = this.route.snapshot.queryParams.returnUrl || '/';
   }
-
-  // convenience getter for easy access to form fields
-  get f() { return this.form.controls; }
-
-  onSubmit() {
-    this.submitted = true;
-
-    // reset alerts on submit
-    this.alertService.clear();
-
-    // stop here if form is invalid
-    if (this.form.invalid) {
-      return;
-    }
-
-    this.loading = true;
-    this.accountService.login(this.f.username.value, this.f.password.value)
-      .pipe(first())
-      .subscribe(
-        data => {
-          this.router.navigate([this.returnUrl]);
-        },
-        error => {
-          this.alertService.error(error);
-          this.loading = false;
-        });
-  }
 }
--- desktop/mantraDesktop/src/login/users/add-edit.component.ts
+++ desktop/mantraDesktop/src/login/users/add-edit.component.ts
@@ -7,7 +7,7 @@ import { AccountService } from 'src/login/_services/account.service';
 import { AlertService } from 'src/login/_services/alert.service';
 
 @Component({ templateUrl: 'add-edit.component.html' })
-export class AddEditComponent implements OnInit {
+export class AddEditComponent {
   form: FormGroup;
   id: string;
   isAddMode: boolean;
@@ -21,82 +21,82 @@ export class AddEditComponent implements OnInit {
     private accountService: AccountService,
     private alertService: AlertService
   ) {}
-
-  ngOnInit() {
-    this.id = this.route.snapshot.params['id'];
-    this.isAddMode = !this.id;
-
-    // password not required in edit mode
-    const passwordValidators = [Validators.minLength(6)];
-    if (this.isAddMode) {
-      passwordValidators.push(Validators.required);
-    }
-
-    this.form = this.formBuilder.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      username: ['', Validators.required],
-      password: ['', passwordValidators]
-    });
-
-    if (!this.isAddMode) {
-      this.accountService.getById(this.id)
-        .pipe(first())
-        .subscribe(x => {
-          this.f.firstName.setValue(x.firstName);
-          this.f.lastName.setValue(x.lastName);
-          this.f.username.setValue(x.username);
-        });
-    }
-  }
-
-  // convenience getter for easy access to form fields
-  get f() { return this.form.controls; }
-
-  onSubmit() {
-    this.submitted = true;
-
-    // reset alerts on submit
-    this.alertService.clear();
-
-    // stop here if form is invalid
-    if (this.form.invalid) {
-      return;
-    }
-
-    this.loading = true;
-    if (this.isAddMode) {
-      this.createUser();
-    } else {
-      this.updateUser();
-    }
-  }
-
-  private createUser() {
-    this.accountService.register(this.form.value)
-      .pipe(first())
-      .subscribe(
-        data => {
-          this.alertService.success('User added successfully', { keepAfterRouteChange: true });
-          this.router.navigate(['.', { relativeTo: this.route }]);
-        },
-        error => {
-          this.alertService.error(error);
-          this.loading = false;
-        });
-  }
-
-  private updateUser() {
-    this.accountService.update(this.id, this.form.value)
-      .pipe(first())
-      .subscribe(
-        data => {
-          this.alertService.success('Update successful', { keepAfterRouteChange: true });
-          this.router.navigate(['..', { relativeTo: this.route }]);
-        },
-        error => {
-          this.alertService.error(error);
-          this.loading = false;
-        });
-  }
+  //
+  // ngOnInit() {
+  //   this.id = this.route.snapshot.params['id'];
+  //   this.isAddMode = !this.id;
+  //
+  //   // password not required in edit mode
+  //   const passwordValidators = [Validators.minLength(6)];
+  //   if (this.isAddMode) {
+  //     passwordValidators.push(Validators.required);
+  //   }
+  //
+  //   this.form = this.formBuilder.group({
+  //     firstName: ['', Validators.required],
+  //     lastName: ['', Validators.required],
+  //     username: ['', Validators.required],
+  //     password: ['', passwordValidators]
+  //   });
+  //
+  //   if (!this.isAddMode) {
+  //     this.accountService.getById(this.id)
+  //       .pipe(first())
+  //       .subscribe(x => {
+  //         this.f.firstName.setValue(x.firstName);
+  //         this.f.lastName.setValue(x.lastName);
+  //         this.f.username.setValue(x.username);
+  //       });
+  //   }
+  // }
+  //
+  // // convenience getter for easy access to form fields
+  // get f() { return this.form.controls; }
+  //
+  // onSubmit() {
+  //   this.submitted = true;
+  //
+  //   // reset alerts on submit
+  //   this.alertService.clear();
+  //
+  //   // stop here if form is invalid
+  //   if (this.form.invalid) {
+  //     return;
+  //   }
+  //
+  //   this.loading = true;
+  //   if (this.isAddMode) {
+  //     this.createUser();
+  //   } else {
+  //     this.updateUser();
+  //   }
+  // }
+  //
+  // private createUser() {
+  //   this.accountService.register(this.form.value)
+  //     .pipe(first())
+  //     .subscribe(
+  //       data => {
+  //         this.alertService.success('User added successfully', { keepAfterRouteChange: true });
+  //         this.router.navigate(['.', { relativeTo: this.route }]);
+  //       },
+  //       error => {
+  //         this.alertService.error(error);
+  //         this.loading = false;
+  //       });
+  // }
+  //
+  // private updateUser() {
+  //   this.accountService.update(this.id, this.form.value)
+  //     .pipe(first())
+  //     .subscribe(
+  //       data => {
+  //         this.alertService.success('Update successful', { keepAfterRouteChange: true });
+  //         this.router.navigate(['..', { relativeTo: this.route }]);
+  //       },
+  //       error => {
+  //         this.alertService.error(error);
+  //         this.loading = false;
+  //       });
+  // }
 }
