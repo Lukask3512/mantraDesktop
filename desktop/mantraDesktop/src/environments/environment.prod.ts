2ee9807b6930edbcbb456e64e3c22a2a01a7071d
Lukáš Obertáš <lukas.obertas@student.tuke.sk>
2020-12-13 21:43:39 +0100

    enviroment.prod fix

--- desktop/mantraDesktop/src/environments/environment.prod.ts
+++ desktop/mantraDesktop/src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:4000'
+  firebase: {
+    apiKey: 'AIzaSyCBZgE7ZP9WGeJtDwtRz3YGDBq4wtQJxF0',
+    authDomain: 'mantra-f994d.firebaseapp.com',
+    databaseURL: 'https://mantra-f994d.firebaseio.com',
+    projectId: 'mantra-f994d',
+    storageBucket: 'mantra-f994d.appspot.com',
+    messagingSenderId: '362332404336',
+    appId: '1:362332404336:web:9f09c665ffda3339c112b5',
+    measurementId: 'G-B2HEF7BWSE',
+  },
+  apiUrl: 'http://prototyp.mantra-online.eu'
 };
