bbfafaa5b2c46a836ab6da552910bfc04cba8b18
Lukáš Obertáš <lukas.obertas@student.tuke.sk>
2021-01-20 18:37:50 +0100

    upraveny log

--- desktop/mantraDesktop/src/app/components/transportation/route-log/route-log.component.ts
+++ desktop/mantraDesktop/src/app/components/transportation/route-log/route-log.component.ts
@@ -31,11 +31,43 @@ export class RouteLogComponent implements OnInit {
 
   toDate(timestamp){
     var date = new Date(parseInt(timestamp));
-    return date.toDateString() + " " + date.getHours()+" : " + date.getMinutes();
+    return date.toDateString() + " " + date.getHours()+":" + date.getMinutes();
   }
 
   ngOnDestroy() {
     this.routeId.unsubscribe();
+  }
+
+  deleteSpecialChar(word){
+    const chars = [...word]
+    for(let i = 0; i < word.length; i++){
+      if(word[i] == "š")
+        chars[i] = 's';
+      if(word[i] == "Š")
+        chars[i] = 's';
+      if(word[i] == "č")
+        chars[i] = 'c';
+      if(word[i] == "Č")
+        chars[i] = 'C';
+      if(word[i] == "ť")
+        chars[i] = 't';
+      if(word[i] == "Ť")
+        chars[i] = 't';
+      if(word[i] == "ž")
+        chars[i] = 'z';
+      if(word[i] == "Ž")
+        chars[i] = 'Z';
+      if(word[i] == "ä")
+        chars[i] = 'a';
+      if(word[i] == "ô")
+        chars[i] = 'o';
+      if(word[i] == "ň")
+        chars[i] = 'n';
+      if(word[i] == "Ň")
+        chars[i] = 'N';
+    }
+    let skusam = chars.join("")
+    return skusam;
   }
 
   downloadAsPDF(){
@@ -43,12 +75,16 @@ export class RouteLogComponent implements OnInit {
 
     const doc: jsPDF = new jsPDF("l", "pt", 'letter');
     var data2 = document.getElementById("routeLog")
-    data2.style.fontSize = '10px'
+    data2.style.fontSize = '9px'
+    data2.style.padding = '2px'
     doc.setFontSize(7);
-    console.log(data2)
+    // console.log(data2)
     doc.html(data2, {
       callback: (doc) => {
         doc.output("dataurlnewwindow");
+        data2.style.fontSize = '16px'
+        data2.style.padding = '8px'
+
       }
     });
   }
